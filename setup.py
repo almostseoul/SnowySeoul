07da43230cf70ef7c39eb64bcc6e40bf53515929
Acheul <72500819+almostseoul@users.noreply.github.com>
2021-03-05 00:17:16 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name = 'chosunpop',
     version = '0.0.1',
+    description = 'chosun Poppop Pop',
     url = "https://github.com/almostseoul/snowyseoul.git",
     author = 'acheul',
     author_email = 'acheul',
